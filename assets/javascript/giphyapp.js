fcf2d831bdd7dbbd963442d60f8743a33e8a5e14
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-03-27 16:59:13 -0500

    fixed add animal input form, updated styling and added download buttons

--- assets/javascript/giphyapp.js
+++ assets/javascript/giphyapp.js
@@ -6,47 +6,58 @@ let animals = ["Elephant", "Duck", "Rhino", "Seahorse", "Kangaroo", "Bat", "Fox"
 
 $(document).ready(function() {
     makeButtons();
+});
 
-    $('.animal-btn').on('click', function() {
-        let baseURL = "http://api.giphy.com/v1/gifs/search?q=";
-        let animalClicked = $(this).val();
-        console.log(animalClicked);
-        let APIkey = "&api_key=Ihu91b8WQG80grTn4IPF3lGNvM8VyD18";
-        let limit = "&limit=";
-        let limitNum = "5";
+
+$(document).on('click', '.animal-btn', function() {
+    let baseURL = "http://api.giphy.com/v1/gifs/search?q=";
+    let animalClicked = $(this).val();
+    console.log(animalClicked);
+    let APIkey = "&api_key=Ihu91b8WQG80grTn4IPF3lGNvM8VyD18";
+    let limit = "&limit=";
+    let limitNum = "5";
+
+    let queryURL = baseURL + animalClicked + APIkey + limit + limitNum;
+    console.log(queryURL);
     
-        let queryURL = baseURL + animalClicked + APIkey + limit + limitNum;
-        
-        $.ajax({url: queryURL, method: "GET"}).then(function(response) {
-            for (let i = 0; i < limitNum; i++) {
-                // console.log(response.data[i].images.fixed_width.url);
-                let animalImg = $('<img>');
-                animalImg.attr('src', response.data[i].images.original_still.url);
-                animalImg.addClass(`${animalClicked} gif-images card-img-top p-0 img-responsive col-md-12 pb-2`);
-                let animalBody = $('<div>');
-                animalBody.addClass('caption pl-3');
-                animalBody.append(`<h3>${animalClicked}</h3><p>testing this out heyyyyyy</p>`)
-                let animalCard = $('<div>');
-                animalCard.addClass('card mt-2');
-                animalCard.append(animalImg);
-                animalCard.append(animalBody);
-                let cardCol = $('<div>');
-                cardCol.addClass('');
-                cardCol.append(animalCard);
-                $('.animal-imgs').prepend(cardCol);
-            }
-        });
-        
+    $.ajax({url: queryURL, method: "GET"}).then(function(response) {
+        for (let i = 0; i < limitNum; i++) {
+            // console.log(response.data[i].images.fixed_width.url);
+            let animalImg = $('<img>');
+            animalImg.attr('src', response.data[i].images.original_still.url);
+            animalImg.addClass(`${animalClicked} gif-images card-img-top p-0 img-responsive col-md-12 pb-2`);
+            let animalBody = $('<div>');
+            animalBody.addClass('caption pl-3');
+            animalBody.append(`<h3>${animalClicked}</h3><p>Rating: ${response.data[i].rating}</p>`);
+            let animalCard = $('<div>');
+            animalCard.addClass('card img-cards mt-2');
+            animalCard.append(animalImg);
+            animalCard.append(animalBody);
+
+            let downloadLink = $('<a download>');
+            downloadLink.attr('target', '_blank');
+            downloadLink.attr("href", response.data[i].images.original.url);
+            let downloadButton = $('<button>');
+            downloadButton.addClass('btn animal-btn mb-3 mx-3');
+            downloadButton.text("Download");
+            downloadLink.append(downloadButton);
+
+            animalCard.append(downloadLink);
+
+            let cardCol = $('<div>');
+            cardCol.addClass('');
+            cardCol.append(animalCard);
+            
+            $('.animal-imgs').prepend(cardCol);
+        }
     });
-
     
-
 });
 
 const  makeButtons = function() {
     for (let i = 0; i < animals.length; i++) {
         let animalButton = $('<button>');
-        animalButton.addClass('btn btn-primary animal-btn my-2 mx-2');
+        animalButton.addClass('btn animal-btn my-2 mx-2');
         animalButton.attr('value', animals[i]);
         animalButton.text(animals[i]);
         $('.animal-buttons').append(animalButton);
@@ -66,4 +77,16 @@ $(document).on('click', 'img', function() {
     $(this).addClass('playing');
     $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
   }
+});
+
+
+$('.new-animal').submit(function() {
+    event.preventDefault();
+    let addInput = $('input').val();
+    let animalButton = $('<button>');
+        animalButton.addClass('btn animal-btn my-2 mx-2');
+        animalButton.attr('value', addInput);
+        animalButton.text(addInput);
+        $('.animal-buttons').append(animalButton);
+    $('input').val('');
 });
